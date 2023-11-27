697d89aacd585a24b612c028ef37c6eedbe188cc
gianni <gianni.deparday@epita.fr>
2023-11-27 18:28:38 +0100

    6

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,10 +1,15 @@
 package fr.lernejo.logger;
 
+import java.util.function.Predicate;
+
 public class LoggerFactory {
     public static Logger getLogger(String name, String filePath) {
         Logger consoleLogger = new ContextualLogger(name, new ConsoleLogger());
-        Logger fileLogger = new ContextualLogger(name, new FileLogger(filePath));
-        return new CompositeLogger(consoleLogger, fileLogger);
+        
+        Predicate<String> simulationFilter = message -> message.contains("Simulation");
+        Logger filteredFileLogger = new FilteredLogger(new ContextualLogger(name, new FileLogger(filePath)), simulationFilter);
+
+        return new CompositeLogger(consoleLogger, filteredFileLogger);
     }
 }
 
